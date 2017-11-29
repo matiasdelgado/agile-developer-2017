32dbd5cb2900dd62ad8db74073549b279be43afd
Matias Delgado <matiasdel@gmail.com>
2017-11-29 15:46:23 -0300

    US-05: Total de issues abiertos

--- test/integration/dashboard_spec.js
+++ test/integration/dashboard_spec.js
@@ -1,3 +1,18 @@
 describe('Dashboard', function() {
+  it('displays the open issues count', function() {
+    cy.resetDB();
+    cy.fixture({
+      status: 'open'
+    });
+    cy.fixture({
+      status: 'open'
+    });
+    cy.fixture({
+      status: 'closed'
+    });
 
+    cy.visit('/dashboard');
+
+    cy.get('[data-test-open-issues]').should('contain', '2 open issues');
+  })
 });

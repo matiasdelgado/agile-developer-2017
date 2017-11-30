d7c96a4ed8883fa090d3c0b324d853b03ba08cdc
Matias Delgado <matiasdel@gmail.com>
2017-11-30 16:56:52 -0300

    Fix lint errors

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const { issueStore } = require('../models');
 
 router.get('/', function(req, res) {
-  const openIssues = issueStore.getAllOpen()
+  const openIssues = issueStore.getAllOpen();
   const openIssuesCount = openIssues.length;
 
-  const highSeverityCount =
-    openIssues.filter(issue => issue.severity === 'High').length;
+  const highSeverityCount = openIssues.filter(issue => issue.severity === 'High').length;
 
   res.render('dashboard', {
     openIssuesCount: openIssuesCount,
